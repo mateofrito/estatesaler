71859ee9aff67a77c27485419361e029ebc792d1
mateofrito <46681151+mateofrito@users.noreply.github.com>
2019-04-19 13:27:06 -0400

    second commit

---
+++ src/main/java/io/mattfry/estatesaler/EstatesalerApplication.java
@@ -0,0 +1,13 @@
+package io.mattfry.estatesaler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstatesalerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstatesalerApplication.class, args);
+	}
+
+}
